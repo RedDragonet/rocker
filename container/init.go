fd4b98ace450d9eade019dd01516581f68d7323c
RedDragonet <aszt0324@gmail.com>
2021-01-05 16:58:46 +0800

    支持 pivot_root
    支持 非Root用户运行

--- container/init.go
+++ container/init.go
@@ -2,24 +2,70 @@ package container
 
 import (
 	"fmt"
-	"github.com/sirupsen/logrus"
 	log "github.com/sirupsen/logrus"
 	"io/ioutil"
 	"os"
 	"os/exec"
+	"path"
+	"path/filepath"
 	"strings"
 	"syscall"
 )
 
 //容器初始化命令
 func NewInitProcess() error {
-	defaultMountFlags := syscall.MS_NOEXEC | syscall.MS_NOSUID | syscall.MS_NODEV
+	log.Infof("当前进程(init)ID %d", os.Getpid())
+	cmdArray := readUserCommand()
+	if cmdArray == nil || len(cmdArray) == 0 {
+		log.Errorf("读取管道参数异常")
+		return fmt.Errorf("读取管道参数异常")
+	}
+	command := cmdArray[0]
+	log.Infof("命令 %s", command)
 
-	//挂载 Proc 目录
-	log.Infof("挂载 Proc 目录")
-	err := syscall.Mount("proc", "/proc", "proc", uintptr(defaultMountFlags), "")
+	err := setUpMount()
 	if err != nil {
-		log.Errorf("挂载 Proc 失败", err)
+		return err
+	}
+
+	command, err = exec.LookPath(command)
+	if err != nil {
+		log.Errorf("命令 %s 查找失败 %v ", command, err)
+		return err
+	}
+	log.Infof("命令查找成功 %s", command)
+
+	log.Infof("syscall.Exec 开始，command=%s", command)
+
+	if err := syscall.Exec(command, cmdArray[0:], os.Environ()); err != nil {
+		log.Errorf("syscall.Exec Error %s", err.Error())
+		return err
+	}
+	return nil
+}
+
+/**
+Init 挂载点
+*/
+func setUpMount() error {
+	pwd, err := os.Getwd()
+	if err != nil {
+		log.Errorf("获取当前工作目录失败 %v", err)
+		return err
+	}
+	log.Infof("当前工作目录是 %s", pwd)
+
+	//需要在 pivotRoot 前挂载 proc，具体的说是 pivotRoot中 unmount putold 之前
+	//由于挂载 Proc 需要 ROOT 权限
+	//由于设置了 CLONE_NEWUSER，运行用户无 ROOT 权限
+	//需要将 CLONE_NEWPID 隔离的进程信息挂载到 newrootfs 中
+	err = mountProc()
+	if err != nil {
+		return err
+	}
+
+	err = pivotRoot(pwd)
+	if err != nil {
 		return err
 	}
 
@@ -29,27 +75,19 @@ func NewInitProcess() error {
 	//	log.Errorf("挂载 devpts 失败", err)
 	//	return err
 	//}
-	cmdArray := readUserCommand()
-	if cmdArray == nil || len(cmdArray) == 0 {
-		log.Errorf("读取管道参数异常 %v", err)
-		return fmt.Errorf("读取管道参数异常 %v", err)
-	}
-	command := cmdArray[0]
-	log.Infof("命令 %s", command)
 
-	command, err = exec.LookPath(command)
-	if err != nil {
-		log.Errorf("命令 %s 查找失败 %v ", command, err)
-		return err
-	}
-	log.Infof("命令查找成功 %s", command)
+	//syscall.Mount("tmpfs", "/dev", "tmpfs", syscall.MS_NOSUID|syscall.MS_STRICTATIME, "mode=755")
 
-	logrus.Infof("syscall.Exec 开始，command=%s", command)
-	if err := syscall.Exec(command, cmdArray[0:], os.Environ()); err != nil {
-		logrus.Errorf("syscall.Exec Error %s", err.Error())
-		return err
-	}
 	return nil
+}
+
+func mountProc() error {
+	//mount proc
+	defaultMountFlags := syscall.MS_NOEXEC | syscall.MS_NOSUID | syscall.MS_NODEV
+
+	//挂载 Proc 目录
+	log.Infof("挂载 Proc 目录")
+	return syscall.Mount("proc", "/proc", "proc", uintptr(defaultMountFlags), "")
 }
 
 func readUserCommand() []string {
@@ -70,8 +108,23 @@ func readUserCommand() []string {
 func NewParentProcess(interactive, tty bool) (*exec.Cmd, *os.File) {
 	//首先调用自己的初始化命令
 	cmd := exec.Command("/proc/self/exe", "init")
+	fmt.Println(os.Getuid(), os.Getgid())
 	cmd.SysProcAttr = &syscall.SysProcAttr{
-		Cloneflags: syscall.CLONE_NEWUTS | syscall.CLONE_NEWIPC | syscall.CLONE_NEWNET | syscall.CLONE_NEWNS | syscall.CLONE_NEWPID,
+		Cloneflags: syscall.CLONE_NEWUTS | syscall.CLONE_NEWIPC | syscall.CLONE_NEWNET | syscall.CLONE_NEWNS | syscall.CLONE_NEWPID | syscall.CLONE_NEWUSER,
+		UidMappings: []syscall.SysProcIDMap{
+			{
+				ContainerID: 0,
+				HostID:      os.Getuid(),
+				Size:        1,
+			},
+		},
+		GidMappings: []syscall.SysProcIDMap{
+			{
+				ContainerID: 0,
+				HostID:      os.Getgid(),
+				Size:        1,
+			},
+		},
 	}
 
 	//新建管道
@@ -96,4 +149,60 @@ func NewParentProcess(interactive, tty bool) (*exec.Cmd, *os.File) {
 		cmd.Stderr = os.Stderr
 	}
 	return cmd, write
+}
+
+//建议参数 http://ifeanyi.co/posts/linux-namespaces-part-3/
+func pivotRoot(rootfs string) error {
+	/* Ensure that 'new_root' and its parent mount don't have
+	   shared propagation (which would cause pivot_root() to
+	   return an error), and prevent propagation of mount
+	   events to the initial mount namespace */
+	if err := syscall.Mount("", "/", "", syscall.MS_PRIVATE|syscall.MS_REC, ""); err != nil {
+		log.Errorf("mount MS_PRIVATE  error ", err)
+		os.Exit(0)
+		return err
+	}
+
+	//将修改 rootfs 挂载点，将rootfs 挂载到当前 mount namespace
+	//满足 pivot_root 要求，old root 和 new root 需要在不同的 file system
+	if err := syscall.Mount(rootfs, rootfs, "", syscall.MS_BIND|syscall.MS_REC, ""); err != nil {
+		log.Errorf("mount rootfs %s error ", rootfs, err)
+		os.Exit(0)
+		return err
+	}
+
+	//新增.putold
+	putold := filepath.Join(rootfs, ".putold")
+	if _, err := os.Stat(putold); !os.IsNotExist(err) {
+		log.Infof("Remove putold %s", putold)
+		_ = os.Remove(putold)
+	}
+
+	if err := os.Mkdir(putold, 0777); err != nil {
+		return err
+	}
+
+	if err := syscall.PivotRoot(rootfs, putold); err != nil {
+		log.Errorf("pivotRoot error %s => %s", rootfs, putold, err)
+		return err
+	}
+
+	//修改工作目录为根节点
+	if err := syscall.Chdir("/"); err != nil {
+		log.Errorf("Chdir error ", err)
+		return err
+	}
+
+	//将老的 rootfs(putold) 移除目录树 umount -l putold
+	if err := syscall.Unmount(path.Join("/", ".putold"), syscall.MNT_DETACH); err != nil {
+		log.Errorf("umount -l .putold error ", err)
+		return err
+	}
+
+	//删除临时目录 .putold
+	if err := os.Remove(path.Join("/", ".putold")); err != nil {
+		log.Errorf("remove .putold error ", err)
+		return err
+	}
+	return nil
 }
